e5352b2fa2c1e271d2c002379ae71f6654021122
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-18 19:12:47 +0300

    some fixes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,28 @@
 import Logo from '../logo/Logo';
 import Container from "@mui/material/Container";
 import classNames from "classnames";
-import styles from "../example/Example.module.scss";
+import styles from "../example/Header.module.scss";
 import Grid from '@mui/material/Grid';
-import { Paper } from '@mui/material';
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import { TextField } from '@mui/material';
+import { InputAdornment } from '@mui/material';
+
+
 export default function Header() {
 
     return (
         <div className={classNames(styles.wrapper)} style={{background: "#0c0c14"}}>
         <Grid container >
         <Logo type='default'/>
+        <TextField type="search" placeholder="Find items, users and activities" 
+        InputProps={{
+     endAdornment: (
+    <InputAdornment position="start" style={{color: 'white'}}>
+    <SearchSharpIcon />
+    </InputAdornment>
+    )
+  }}
+       />
             <Grid item>
             <Container >Home</Container>
             </Grid>
