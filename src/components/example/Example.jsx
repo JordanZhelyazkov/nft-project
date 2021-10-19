49e0f2c0947b6298a0ed3898592f87e7648668e3
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-19 13:48:30 +0300

    fixes

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,7 +3,6 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
-import Logo from '../logo/Logo';
 import Header from "../header/Header";
 
 export default function Copyright() {
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,35 +4,37 @@ import classNames from "classnames";
 import styles from "./Header.module.scss";
 import Grid from '@mui/material/Grid';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
-import { TextField } from '@mui/material';
-import { InputAdornment } from '@mui/material';
+import { IconButton, TextField } from '@mui/material';
+
 
 
 export default function Header() {
-
+    
     return (
-        <div className={classNames(styles.wrapper)} style={{background: "#0c0c14"}}>
-        <Grid container >
+        <div className={classNames(styles.wrapper)}>
         <Logo type='default'/>
-        <TextField type="search" placeholder="Find items, users and activities" 
+        <TextField  type="search" placeholder="Find items, users and activities" 
         InputProps={{
      endAdornment: (
-    <InputAdornment position="start" style={{color: 'white'}}>
-    <SearchSharpIcon />
-    </InputAdornment>
-    )
-  }}
-       />
-            <Grid item>
-            <Container >Home</Container>
+    <IconButton >
+    <SearchSharpIcon style={{color: 'white'}}  />
+    </IconButton>
+    ) } } />
+        <Grid container
+     direction="row"
+     justifyContent="flex-end"
+     alignItems="center"
+        >
+            <Grid item xs>
+            Home
             </Grid>
-            <Grid item>
-            <Container>Activity</Container>
+            <Grid item xs>
+            Activity
             </Grid>
-            <Grid item>
-            <Container>EXPLORE</Container>
+            <Grid item xs>
+            EXPLORE
             </Grid>
         </Grid>
-        </div>
+    </div>
     )
 }
