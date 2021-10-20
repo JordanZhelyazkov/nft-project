0085a0ce05e41017ab507c88524d06ec10ad931d
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-20 14:25:04 +0300

    fixed true and false

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -4,25 +4,19 @@ import VerifiedIcon from '@mui/icons-material/Verified';
 import { useState, useEffect } from 'react';
 
 
-export default function Avatar({url, size = 90, verified = "false"}) {
+export default function Avatar({url, size = 90, verified = false}) {
 const [isVerified, setVerified] = useState('');
 
 useEffect(() => {
     if(verified){
         setVerified(<div><VerifiedIcon/></div>)
     } 
-}, [verified])
+}, [isVerified])
 
 return (
   <div className="avatar"  style={{width: size + 'px', height: size + 'px'}} > 
-  
       <img className="image"  style={{width: "100%", justifyContent: 'center'}} src={url}></img>
-      {isVerified}
-      
-      
-      
-      
+      {isVerified} 
   </div>
 )
 }
-// 
