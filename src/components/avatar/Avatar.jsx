0d7f29ec3a665f21af439701e4f5d0a07d42be94
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-20 12:11:29 +0300

    fixes

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -3,19 +3,13 @@ import { Badge } from '@mui/icons-material';
 import classNames from 'classnames';
 
 import { useEffect, useState } from 'react';
-export default function Avatar({url, size, verified}) {
+export default function Avatar({url, size = 90, verified = false}) {
 
 const badge = '/images/verified.svg';
 return (
   <div className="avatar"  style={{width: size + 'px', height: size + 'px'}} >
       <img className="image" style={{width: "100%", justifyContent: 'center'}} src={url}></img>
-      <Badge className="badge" src={`${verified === true ? badge : ''}`}></Badge>
+      <Badge className="badge" >{`${verified === true ? badge : ''}`}</Badge>
   </div>
 )
-
-}
-
-Avatar.defaultProps = {
-    size: "90",
-    verified: true
 }
