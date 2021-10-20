e2f3b51d0429902c240aacec609302ab31c255bf
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-20 20:09:48 +0300

    fixes

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 import Badge from '@mui/material/Badge';
 
 
-export default function Avatar({size = 90, url, verified = false}) {
+export default function Avatar({size = 90, url, verified = true}) {
 // const [isVerified, setVerified] = useState('');
 
 // useEffect(() => {
--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -16,7 +16,7 @@ export default function Copyright() {
       <Header />
       
       <Container className={classNames(styles.container)} maxWidth="xl">
-      <Avatar  size={90} url="/images/avatar.png" verified={true}/>
+      <Avatar url="/images/avatar.png"/>
 
         <Paper className={classNames(styles.paper)}>
        
