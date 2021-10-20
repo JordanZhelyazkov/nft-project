963b9f2131aa36653fac0c0289e1e16044a8b0d4
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-20 19:57:42 +0300

    fixes

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,6 +1,6 @@
 import styles from './Avatar.module.scss';
 import classNames from 'classnames';
-import { useState, useEffect } from 'react';
+
 import Badge from '@mui/material/Badge';
 
 
@@ -23,13 +23,11 @@ export default function Avatar({size = 90, url, verified = false}) {
 return (
   <div className="avatar"  style={{width: size + 'px', height: size + 'px'}} > 
     <img className="image"  style={{width: "100%", justifyContent: 'center'}} src={url}></img>
-          {/* <Badge className='badge'
-          
-          style={{display: `${verified ? 'block' : 'none'}`}}
-          color="secondary"
-        > */}
-    <img className='badge' src="/images/verified.svg" style={{display: `${verified ? 'block' : 'none'}`}}></img>
-        {/* </Badge> */}
+        <Badge className='badge'
+        style={{display: `${verified ? 'block' : 'none'}`}}
+        >
+        <img  src="/images/verified.svg" style={{display: `${verified ? 'block' : 'none'}`}}></img>
+        </Badge>
   </div>
 )
 }
