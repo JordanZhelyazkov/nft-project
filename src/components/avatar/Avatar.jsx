952732800edeb63e213d5b3249aa845237dd2047
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-21 15:45:44 +0300

    fixes img badge

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,35 +1,21 @@
 import styles from './Avatar.module.scss';
-import Badge from '@mui/material/Badge';
+// import Badge from '@mui/material/Badge';
 import VerifiedIcon from '@mui/icons-material/Verified';
 
 
 
 
-export default function Avatar({size = 90, url, verified = false}) {
-// const [isVerified, setVerified] = useState('');
 
-// useEffect(() => {
-//     if(verified){
-//         setVerified(<div><VerifiedIcon/></div>)
-//     } 
-// }, [isVerified])
-// const isVerified = () => {
-//     if(verified){
-//         const ver = <VerifiedIcon/>
-//         return ver;
-//     } 
-// }
+export default function Avatar({size = 90, url, verified = false}) {
 
 
 return (
   <div className="avatar"  style={{width: size + 'px', height: size + 'px'}} > 
-    
-        
         <img className="image"  style={{width: "100%", justifyContent: 'center'}} src={url} ></img>
-        <VerifiedIcon className='badge' style={{display: `${verified ? 'block' : 'none'}`}}/>
-        
+        {verified && <img src="../images/verified.svg"/>}
   </div>
 )
 }
+// style={{display: `${verified ? 'block' : 'none'}`}}
 // style={{display: `${verified ? 'block' : 'none'}`}}
 //<img  src="/images/verified.svg" style={{display: `${verified ? 'block' : 'none'}`}}></img> 
