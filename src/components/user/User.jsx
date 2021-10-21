3bab15085182a9ab086c5dd61b5b873c48fa7983
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-21 23:02:22 +0300

    fixes

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -3,10 +3,10 @@ import styles from"./User.module.scss";
 
 export default function User({name="", info="", avatar='', size=55, verified=false}){
 return (
-<div className="user">
+<div className={styles.user}>
 <Avatar url={avatar}/>
-<p className="name">{name}</p>
-<p className="info">{info}</p>
+<p className={styles.name}>{name}</p>
+<p className={styles.info}>{info}</p>
 </div>
 )
 }
