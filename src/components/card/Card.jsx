6d743ba779bab1e415a36d70a7989842903c4ff9
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-22 23:13:55 +0300

    changes

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,16 +3,19 @@ import { Chip } from "@mui/material";
 import Avatar from "../avatar/Avatar";
 import User from "../user/User";
 import millify from "millify";
-export default function Card({name="", likes=0, mediaUrl="", avatarUrl="", verified=false, price="", currency=""}){
+export default function Card({name="", likes=0, mediaUrl="", avatarUrl="", verified=false, price="", currency=""}) {
   
     
     return (
     <div className="card">
+        <User mediaUrl={mediaUrl} className="media"/>
         <title className="title">{name}</title>
-        <Chip label="Clickable" onClick={() => likes+1}><button className="likes">{millify(likes)}</button></Chip>
-        <Avatar mediaUrl={<img src={mediaUrl} className="media"></img>}/>
-        <User avatarUrl={avatarUrl} verified={false}/>
-        <price className="price">{price}</price>
+        <p className="price">{price}</p>
+        <Chip label="Clickable" onClick={() => likes+1}><button className="likes">{millify(likes, {
+  units: ["K"],
+  space: true,
+})}</button></Chip>
+        <Avatar avatarUrl={avatarUrl} verified={false}/>
     </div>
     )
 }
