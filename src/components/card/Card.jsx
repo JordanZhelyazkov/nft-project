ece29978676d3160464ea7332f1cd036bc355f3e
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-25 12:03:51 +0300

    fixes

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,18 +2,16 @@ import styles from "./Card.module.scss";
 import { Chip } from "@mui/material";
 import Avatar from "../avatar/Avatar";
 import millify from "millify";
+import "./Card.module.scss";
 
-export default function Card({name, likes, mediaUrl, avatarUrl, verified, price, currency}) {
+export default function Card({name, likes=56553, mediaUrl, avatarUrl, verified, price, currency}) {
   
-    const handleClick = (likes) => {
-     return likes++;
-    }
     
     return (
-    <div className="card">
+    <div className="card" >
         <Avatar url={avatarUrl} ifVerified={verified}></Avatar>
         <img src={mediaUrl} className="media"></img>
-        <p className="title">{name}</p>
+        <h1 className="title">{name}</h1>
         <p className="price">{price} {currency}</p>
         <button className="likes"><Chip label={millify(likes)}></Chip></button>
     </div>
--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -7,7 +7,7 @@ import Header from "../header/Header";
 import User from "../user/User";
 import Avatar from "../avatar/Avatar";
 import Card from "../card/Card";
-import { boolean } from "mathjs";
+
 
 
 
@@ -19,10 +19,10 @@ export default function Copyright() {
       <Header />
       <Avatar url={'../images/avatar.png'}/>
       <User/>
-      <Card name="Gorcho" likes={65} mediaUrl="../images/nft.jpg" avatarUrl="../images/avatar.png" verified={true} price="64731" currency="BGN"/>
+      
       <Container className={classNames(styles.container)} maxWidth="xl">
       
-
+      <Card name="Gorcho"  mediaUrl="../images/nft.jpg" avatarUrl="../images/avatar.png" verified={true} price="64731" currency="ETH"/>
         <Paper className={classNames(styles.paper)}>
        
           <Link href="/about" color="secondary">
