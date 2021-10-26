65ad2ae8af412406dbe9d21d85650205ae95179c
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-26 09:50:52 +0300

    fixes

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,7 +7,6 @@ import { Card } from "@mui/material";
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 export default function CardComponent({
   name,
