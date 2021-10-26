6a86d85fabd313ed9caa902a8362065656d01546
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-26 09:18:01 +0300

    fixes

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,24 +4,43 @@ import millify from "millify";
 import classNames from 'classnames';
 import styles from"./Card.module.scss";
 import { Card } from "@mui/material";
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 export default function CardComponent({
   name,
   likes = 1000,
   mediaUrl,
-  user= { avatarUrl, verified },
+  user = { avatarUrl, verified },
   price,
   currency,
 }) {
   return (
-    <Card className={classNames(styles.card, styles.container)}>
+    <Card className={classNames(styles.card, styles.container)} sx={{ maxWidth: 345 }}>
           <Avatar
             url={user.avatarUrl}
             verified={user.verified}
           ></Avatar>
-        <img src={mediaUrl} className={classNames(styles.media)}></img>
-        <h3 className="title">{name}</h3>
-        <p className={classNames(styles.price)}>{price} {currency}</p>
-        <Chip  className={classNames(styles.likes)} label={millify(likes)} variant="outlined" />
+        {/* <img src={mediaUrl} className={classNames(styles.media)}></img> */}
+        <CardMedia
+        className={classNames(styles.media)}
+        component="img"
+        image={mediaUrl}
+      />
+      <CardContent>
+        <Typography className={classNames(styles.title)} gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography className={classNames(styles.price)} variant="body2" color="text.secondary">
+          {price} {currency}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Chip label={millify(likes)} variant="outlined" className={classNames(styles.likes)} ></Chip>
+      </CardActions>
+        {/* <Chip className={classNames(styles.likes)} label={millify(likes)} variant="outlined" /> */}
     </Card>
   );
 }
