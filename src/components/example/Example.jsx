edaaa9a553bc5155fbb0a07f15528a7e7eb9678f
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-28 16:49:16 +0300

    fixes

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -11,7 +11,7 @@ export default function Copyright() {
     
     <div className={classNames(styles.wrapper)}>
       <Header />
-      <Trending cards={}/>
+      <Trending />
       <Container className={classNames(styles.container)} maxWidth="xl">
       
         <Paper className={classNames(styles.paper)}>
@@ -24,3 +24,31 @@ export default function Copyright() {
     </div>
   );
 }
+// cards={[{
+//   name: "Ivy",
+//   user: {avatarUrl: './images/avatar.png', verified: true},
+//   mediaUrl: './images/nft.jpg',
+//   price: 1,
+//   currency: 'ETH',
+// },
+// {
+// name: "pe6o",
+// user: {avatarUrl: './images/avatar.png', verified: true},
+// mediaUrl: './images/nft.jpg',
+// price: 1,
+// currency: 'ETH',
+// },
+// {
+// name: "go6o",
+// user: {avatarUrl: './images/avatar.png', verified: true},
+// mediaUrl: './images/nft.jpg',
+// price: 1,
+// currency: 'ETH',
+// },
+// {
+// name: "Ivan",
+// user: {avatarUrl: './images/avatar.png', verified: true},
+// mediaUrl: './images/nft.jpg',
+// price: 1,
+// currency: 'ETH',
+// }]}
