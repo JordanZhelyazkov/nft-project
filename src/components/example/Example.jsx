a8ca90aa8a0628328962e307d806b75d7a776d65
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-28 15:46:08 +0300

    fix

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -11,34 +11,7 @@ export default function Copyright() {
     
     <div className={classNames(styles.wrapper)}>
       <Header />
-      <Trending cards={[{
-    name: "Ivy",
-    user: {avatarUrl: './images/avatar.png', verified: true},
-    mediaUrl: './images/nft.jpg',
-    price: 1,
-    currency: 'ETH',
-},
-{
-  name: "pe6o",
-  user: {avatarUrl: './images/avatar.png', verified: true},
-  mediaUrl: './images/nft.jpg',
-  price: 1,
-  currency: 'ETH',
-},
-{
-  name: "go6o",
-  user: {avatarUrl: './images/avatar.png', verified: true},
-  mediaUrl: './images/nft.jpg',
-  price: 1,
-  currency: 'ETH',
-},
-{
-  name: "Ivan",
-  user: {avatarUrl: './images/avatar.png', verified: true},
-  mediaUrl: './images/nft.jpg',
-  price: 1,
-  currency: 'ETH',
-}]}/>
+      <Trending />
       <Container className={classNames(styles.container)} maxWidth="xl">
       
         <Paper className={classNames(styles.paper)}>
@@ -51,3 +24,31 @@ export default function Copyright() {
     </div>
   );
 }
+// cards={[{
+//   name: "Ivy",
+//   user: {avatarUrl: './images/avatar.png', verified: true},
+//   mediaUrl: './images/nft.jpg',
+//   price: 1,
+//   currency: 'ETH',
+// },
+// {
+// name: "pe6o",
+// user: {avatarUrl: './images/avatar.png', verified: true},
+// mediaUrl: './images/nft.jpg',
+// price: 1,
+// currency: 'ETH',
+// },
+// {
+// name: "go6o",
+// user: {avatarUrl: './images/avatar.png', verified: true},
+// mediaUrl: './images/nft.jpg',
+// price: 1,
+// currency: 'ETH',
+// },
+// {
+// name: "Ivan",
+// user: {avatarUrl: './images/avatar.png', verified: true},
+// mediaUrl: './images/nft.jpg',
+// price: 1,
+// currency: 'ETH',
+// }]}
