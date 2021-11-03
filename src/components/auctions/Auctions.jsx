91b46f105f8d23ce61ca8947084b571b20365526
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-11-03 19:39:21 +0200

    fixes

---
+++ src/components/auctions/Auctions.jsx
@@ -0,0 +1,50 @@
+import Card from "../card/Card";
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import styles from"./Auctions.module.scss";
+import Container from '@mui/material/Container';
+
+export default function Auction({cards = []}){
+
+    
+    const [period, setPeriod] = useState('');
+
+    const handleChange = (event) => {
+    setPeriod(event.target.value);
+  }; 
+
+    const mappedCards = cards.map(card => <Card name={card.name} avatarUrl={card.user.avatarUrl}
+        verified={card.user.verified} mediaUrl={card.mediaUrl} price={card.price} currency={card.currency} timeLeft={card.timeLeft}/>)
+
+    return (
+        <Container >  
+        <div>
+        <h3>Live auctions</h3>
+        <Box className="select" sx={{ minWidth: 120, maxWidth: 150, float: "right"}}>
+         <FormControl fullWidth>
+         <InputLabel id="demo-simple-select-label"></InputLabel>
+          <Select
+            id="select"
+            value={period}
+            label="Period"
+            onChange={handleChange}>
+          <MenuItem value="This Month">This Month</MenuItem>
+          <MenuItem value="This Week">This Week</MenuItem>
+          <MenuItem value="Today">Today</MenuItem>
+          </Select> 
+          </FormControl>
+     </Box >
+     </div>
+     <div style={{width: '100%'}}>
+     <Box >
+     <Grid container  className="gridContainer" gap={2} alignItems="center">
+       {mappedCards}   
+     </Grid>
+     </Box>
+     </div>
+      </Container>
+    )
+}
