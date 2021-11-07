f19d7cf9eae41aebd373367604bc592f6e8089e3
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-11-07 17:31:30 +0200

    fixes

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -17,7 +17,16 @@ export default function Copyright() {
       <Header />
       <Trending cards={[]}/>
       <Auctions cards={[]}/>
-      <How 
+      <How description="this is a card" 
+      title="this is the title" 
+      items={[{
+        title: "ivan i go6o",
+        description: "this is ivan i go6o"
+      },{
+        title: "pe6o i to6o",
+        description: "pesho i tosho team"
+    }]}
+      link="https://bg.wikipedia.org/wiki/SVG#/media/%D0%A4%D0%B0%D0%B9%D0%BB:SVG_Logo.svg"
       
       />
       <Container className={classNames(styles.container)} maxWidth="xl">
@@ -34,13 +43,3 @@ export default function Copyright() {
   );
 }
 
-// description="this is a card" 
-//       title="this is the title" 
-//       items={[{
-//         title: "ivan i go6o",
-//         description: "this is ivan i go6o"
-//       },{
-//         title: "pe6o i to6o",
-//         description: "pesho i tosho team"
-//     }]}
-//       link="https://bg.wikipedia.org/wiki/SVG#/media/%D0%A4%D0%B0%D0%B9%D0%BB:SVG_Logo.svg"
--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -2,22 +2,23 @@ import Grid from "@mui/material/Grid";
 import Item from '@mui/material/ListItemAvatar';
 import Step from "../step/Step";
 import styles from "./How.module.scss";
+import classNames from "classnames";
+import Paper from '@mui/material/Paper';
 
-export default function How({description, title, items=[], link}){
-const mappedItems = items.map((it, index) => {
-    <Step number={index} title={it.title} description={it.description}/>
-})
+
+
+export default function How({description, title, items, link}){
+const mappedItems = items.map((it, index) => 
+<Step number={index} title={it.title} description={it.description}/>)
     return (
-    <div>
-        <Grid container className="container" style={{display: 'flex', flexDirection: "column"}}>
+        <div className={classNames(styles.wrapper,styles.container, styles.paper)} >
+        <Grid container  style={{display: 'flex', flexDirection: "column", justifyContent: 'flex-start'}}>
             <Grid item><Item>{title}</Item></Grid>
             <Grid item><Item>{description}</Item></Grid>
             <Grid item><Item><button href={link}>Learn more</button></Item></Grid>
-            {mappedItems}
+            {mappedItems} 
         </Grid>
-       
-        
-        
+          
     </div>
     )
 }
