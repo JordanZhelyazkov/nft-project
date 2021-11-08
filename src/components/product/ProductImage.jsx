1445fcc93acf7f35eef433eb4baa27db229530d6
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-11-08 12:58:19 +0200

    fixes

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -1,12 +1,12 @@
 import styles from "./ProductImage.module.scss";
-import classNames from "classnames";
+
 
 export default function ProductImage({url}){
 
     return (
       <div>
-          <div className={classNames(styles.product-image)}>
-          <img className={classNames(styles.image)} src={url}/>
+          <div style={styles.product-image}>
+          <img style={styles.image} src={url}/>
           </div>
       </div>
     )
