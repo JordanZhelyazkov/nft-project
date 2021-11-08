47e9a1435d4bb9f2255a283fa69f44fe2ebb787f
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-11-08 23:15:49 +0200

    fixes

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -2,17 +2,39 @@ import styles from "./ProductInfoTimer.module.scss";
 import Countdown from 'react-countdown';
 
 export default function ProductInfoTimer({timeEnd, onTimeEnd}){
-    if(timeEnd == null) {
-        return (
-            <div></div>
+    const renderer = ({hours, minutes, seconds, completed}) => {
+    if(completed){
+        return(
+        onTimeEnd
         )
     } else {
-    return(
-     <div className={styles["product-info-timer"]}>
+        return (
+        <span>
+        {hours}:{minutes}:{seconds}
+      </span>
+        )
+    }
+}
+
+ const returnFunc = () => {
+     if(timeEnd === null) {
+         return (
+             <div></div>
+         )
+     } else {
+         return (
+       <div className={styles["product-info-timer"]}>
        <div className={styles["timer"]}>
-       <Countdown date={Date.now() + timeEnd} />
+       <Countdown date={Date.now() + onTimeEnd} renderer={renderer} />
+       <label className={styles["title"]}>Ends in: {timeEnd}</label>
        </div>
      </div>
+         )
+     }
+ }
+    
+    return(
+        {returnFunc}
     )
-    }
+    
 }
