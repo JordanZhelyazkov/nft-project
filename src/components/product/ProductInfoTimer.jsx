c92c5276a279bdc9061fb172422d38508da727c8
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-11-09 10:51:22 +0200

    fixes

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -15,27 +15,14 @@ export default function ProductInfoTimer({ timeEnd, onTimeEnd }) {
 //       );
 //     }
 //   };
-  const isNull = () => {
-      const disValue;
-      if(timeEnd){
-          disValue = {display: "block"};
-          return disValue;
-      } else {
-          disValue = {display: "none"};
-          return disValue
-      }
-  }
  
-
   return (
     <div className={styles["product-info-timer"]}>
     <div className={styles["timer"]} >
     <label className={styles["title"]}>Ends in: {timeEnd}</label>
       <Countdown
         onComplete={onTimeEnd}
-        style={isNull}
         date={Date.now() + onTimeEnd}
-        // renderer={renderer}
       />
     </div>
     </div>
