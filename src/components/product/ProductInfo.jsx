1ed00370bb6efafeb3bd0297233e26b7754e63a3
unknown <jordanasenovzhelyazkov@gmail.com>
2021-11-10 19:08:29 +0200

    fixes

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -5,38 +5,57 @@ import ProductInfoStatus from "./ProductInfoStatus";
 import ProductInfoLikes from "./ProductInfoLikes";
 import ProductInfoCreator from "./ProductInfoCreator";
 import ProductInfoTimer from "./ProductInfoTimer";
-import Stack from '@mui/material/Stack';
-import Grid from '@mui/material/Grid';
+import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
 
-
-
-
-export default function ProductInfo({title, creator, price, currency, likes, onTimeEnd, timeEnd, isLive}){
-
-    return(
-        <div className={styles["product-info"]}>
-        <h1><ProductInfoTitle text={title}></ProductInfoTitle></h1>
-         <Stack className={styles["stats"]} direction="row" spacing={4}>
-             <ProductInfoPrice amount={price} currency={currency}> </ProductInfoPrice>
-             <ProductInfoLikes amount={likes}></ProductInfoLikes>
-         </Stack>
-         <Grid container 
-         display="flex" 
-         direction="row" 
-         justifyContent="center"
-         alignItems="center"
+export default function ProductInfo({
+  title,
+  creator,
+  price,
+  currency,
+  likes,
+  onTimeEnd,
+  timeEnd,
+  isLive,
+}) {
+  return (
+    <div className={styles["product-info"]}>
+      <h1>
+        <ProductInfoTitle text={title}></ProductInfoTitle>
+      </h1>
+      <Stack className={styles["stats"]} direction="row" spacing={4}>
+        <ProductInfoPrice amount={price} currency={currency}>
+          {" "}
+        </ProductInfoPrice>
+        <ProductInfoLikes amount={likes}></ProductInfoLikes>
+      </Stack>
+      <Grid
+        container
+        display="flex"
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
         //  style={{aspectRatio: "7/5"}}
-        >
-            
-             <Grid item className={styles["grid-item1"]} >
-                 <ProductInfoCreator name={creator.name} avatar={creator.avatar}></ProductInfoCreator>
-                 </Grid>
-                 <Grid item className={styles["grid-item2"]}>
-                 <ProductInfoTimer timeEnd={timeEnd} onTimeEnd={onTimeEnd}></ProductInfoTimer>
-                 </Grid>
-             </Grid>
-         
-         {isLive && <p><ProductInfoStatus></ProductInfoStatus></p>}
-        </div>
-    )
+      >
+        <Grid item style={{width:"200px", height: "200px" ,aspectRatio: "6/5"}}>
+          <ProductInfoCreator
+            name={creator.name}
+            avatar={creator.avatar}
+          ></ProductInfoCreator>
+        </Grid>
+        <Grid item style={{width:"200px", height: "200px" ,aspectRatio: "7/5"}}>
+          <ProductInfoTimer
+            timeEnd={timeEnd}
+            onTimeEnd={onTimeEnd}
+          ></ProductInfoTimer>
+        </Grid>
+      </Grid>
+
+      {isLive && (
+        <p>
+          <ProductInfoStatus></ProductInfoStatus>
+        </p>
+      )}
+    </div>
+  );
 }
