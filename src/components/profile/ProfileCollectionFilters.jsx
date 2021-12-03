a0f0b87b44540610f255a3d8e47ab35f0537c54f
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-03 22:44:08 +0200

    trying to fix input props value of variant standard again

--- src/components/profile/ProfileCollectionFilters.jsx
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -18,7 +18,7 @@ export default function ProfileCollectionFilters({ filters }) {
                     <MenuItem value=""></MenuItem>
                 </Select>
 
-                <TextField  variant="standard" />
+                <TextField inputProps={{variant: "standard"}}  />
                 
            
         </div>
