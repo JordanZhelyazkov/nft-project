a123e75bc565486c394cccc8ec44bc922114c416
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-04 08:50:24 +0200

    adding search icon to the textfield and variant to the textfield

--- src/components/profile/ProfileCollectionFilters.jsx
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -1,6 +1,7 @@
-import {  Select, InputLabel, MenuItem, TextField } from "@mui/material";
+import {  Select, InputLabel, MenuItem, TextField, Stack } from "@mui/material";
 import classNames from "classnames";
 import styles from "./ProfileCollectionFilters.module.scss";
+import SearchIcon from '@mui/icons-material/Search';
 
 
 export default function ProfileCollectionFilters({ filters }) {
@@ -8,17 +9,32 @@ export default function ProfileCollectionFilters({ filters }) {
     return (
         <div className={classNames(styles['profile-collection-filters'])}>
            
-                <InputLabel>Sort by</InputLabel>
+           <InputLabel>Sort by</InputLabel>
                 <Select>
-                    <MenuItem value=""></MenuItem>
+                    <Stack>
+                    <MenuItem value="1">Ascending</MenuItem>
+                    <MenuItem value="2">Descending</MenuItem>
+                    </Stack>
                 </Select>
-
+                
                 <InputLabel>Price range</InputLabel>
                 <Select>
-                    <MenuItem value=""></MenuItem>
+                    <Stack>
+                    <MenuItem value="3">0.3 - 0.5 ETH</MenuItem>
+                    <MenuItem value="4">0.5 - 2 ETH</MenuItem>
+                    <MenuItem value="5">2- 3 ETH</MenuItem>
+                    </Stack>
                 </Select>
 
-                <TextField inputProps={{variant: "standard"}}  />
+                <TextField 
+               
+                InputProps={{
+                    variant: "standard",
+                    startAdornment: (
+                    <SearchIcon />
+                    )
+                }}
+                />
                 
            
         </div>
