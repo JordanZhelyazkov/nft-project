aae75f7d9919e45856ef33d0d11a03c19b8f310f
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-04 13:15:43 +0200

    fixes on the variant of the textfield and adding color background_paper

--- src/components/profile/ProfileCollectionFilters.jsx
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -1,42 +1,40 @@
-import {  Select, InputLabel, MenuItem, TextField, Stack } from "@mui/material";
+import { Select, InputLabel, MenuItem, TextField, Stack, InputAdornment } from "@mui/material";
 import classNames from "classnames";
 import styles from "./ProfileCollectionFilters.module.scss";
 import SearchIcon from '@mui/icons-material/Search';
 
 
 export default function ProfileCollectionFilters({ filters }) {
-    
+
     return (
         <div className={classNames(styles['profile-collection-filters'])}>
-           
-           <InputLabel>Sort by</InputLabel>
-                <Select>
-                    <Stack>
+
+            <InputLabel>Sort by</InputLabel>
+            <Select>
+                <Stack>
                     <MenuItem value="1">Ascending</MenuItem>
                     <MenuItem value="2">Descending</MenuItem>
-                    </Stack>
-                </Select>
-                
-                <InputLabel>Price range</InputLabel>
-                <Select>
-                    <Stack>
+                </Stack>
+            </Select>
+
+            <InputLabel>Price range</InputLabel>
+            <Select>
+                <Stack>
                     <MenuItem value="3">0.3 - 0.5 ETH</MenuItem>
                     <MenuItem value="4">0.5 - 2 ETH</MenuItem>
                     <MenuItem value="5">2- 3 ETH</MenuItem>
-                    </Stack>
-                </Select>
+                </Stack>
+            </Select>
 
-                <TextField 
-               
+            <TextField className={classNames(styles["text-field"])}
                 InputProps={{
-                    variant: "standard",
                     startAdornment: (
-                    <SearchIcon />
+                        <InputAdornment  variant="standard" >
+                            <SearchIcon sx={{ color: "#E1E1FC" }}/>
+                        </InputAdornment>
                     )
                 }}
-                />
-                
-           
+            />
         </div>
     )
 }
