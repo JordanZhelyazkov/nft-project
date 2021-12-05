60f51d3c4ae45f776c79282cf5cc2c12e51f7848
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-12-05 22:16:25 +0200

    creating a profile page

---
+++ pages/profile/[id]/index.jsx
@@ -0,0 +1,31 @@
+import Header from "../../../src/components/header/Header";
+import ProfileHero from "../../../src/components/profile/ProfileHero";
+import ProfileUser from "../../../src/components/profile/ProfileUser";
+import ProfileCollection from "../../../src/components/profile/ProfileCollection";
+import Footer from "../../../src/components/footer/Footer";
+import profilesData from "../../../public/data/profile.json";
+import filtersData from "../../../public/data/filtersProfile.json";
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+
+
+export default function Profile(){
+
+    const [idProfile, setIdProfile] = useState({});
+    const router = useRouter();
+    const { id } = router.query;
+
+    useEffect(() => {
+        profilesData.filter(el => el.id == id ? setIdProfile(el) : "")
+    }, [id])
+
+    return (
+        <div>
+         <Header />
+         <ProfileHero image={profilesData.avatar.backgroundUrl}/>
+         <ProfileUser name={profilesData.username} info={profilesData.info} avatar={profilesData.avatar} verified={profilesData.verified}/>
+         <ProfileCollection   filters={...filtersData} items={profilesData.nfts}/>
+         <Footer />
+        </div>
+    )
+}
