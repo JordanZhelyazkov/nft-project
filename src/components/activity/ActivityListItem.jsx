c332a0085f3d8e5bdfc9af8b90cf8b9d0e2470e7
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2022-01-16 15:12:31 +0200

    added the following props to Avatar and also link to the current nft user name and user

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -1,16 +1,19 @@
 import styles from "./ActivityListItem.module.scss";
 import classNames from "classnames";
 import Avatar from "../avatar/Avatar";
-import Link from "../link/Link";
+import Link from "react-router-dom";
 import { parseISO, formatDistance } from 'date-fns';
 
 
-export default function ActivityItem({ user, created_at, nft, type = "like" }) {
+
+export default function ActivityListItem({ user, created_at, nft, type = "like" }) {
 
     return (
         <div className={classNames(styles['activity-item'])}>
-            <Avatar {...user}/>
-            <p><Link to={user}>{user.name}</Link>{type === "like" ? "likes" : "bought"}<Link to={nft}>{nft.name}</Link></p>
+            <Avatar size="10px" url={user.avatarUrl} verified={user.verified} />
+            <p>{user.name}{type === "like" ? "likes" : "bought"}<Link to={`nft/${nft.name}`}>{nft.name}</Link>
+            <Link to={`nft/user/${nft.user.name}`}>{nft.user.name}</Link>
+            </p>
             <p>{formatDistance(parseISO(created_at), new Date(), {
                     addSuffix: true,
                 })}</p>
