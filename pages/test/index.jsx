a9ad882c6502c517cf303896d72cf180500f3483
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2022-01-29 12:19:00 +0200

    created href to the link component for redirecting when clicked and formatdistance for showing the correct date when nft was likes

---
+++ pages/test/index.jsx
@@ -0,0 +1,15 @@
+// import ActivityListItem from "../../src/components/activity/ActivityListItem";
+// import activity from "../../public/data/activity.json";
+
+
+// export default function Activity() {
+
+//    const activities = activity.map((item, id) => 
+//         <ActivityListItem key={id} user={item.user} created_at={item.created_at} nft={item.nft} type={item.type} /> )     
+
+//     return (
+//         <div>
+//             {activities}
+//         </div>
+//     )
+// }
--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -1,22 +1,22 @@
 import styles from "./ActivityListItem.module.scss";
 import classNames from "classnames";
 import Avatar from "../avatar/Avatar";
-import Link from "react-router-dom";
+import Link from "../link/Link";
 import { parseISO, formatDistance } from 'date-fns';
 
 
-
-export default function ActivityListItem({ user, created_at, nft, type = "like" }) {
-
+export default function ActivityListItem({ 
+    user,
+    created_at,
+    nft,
+    type = "like" }) {
+    
     return (
         <div className={classNames(styles['activity-item'])}>
-            <Avatar url={user.avatarUrl} verified={user.verified} />
-            <p>{user.name}{type === "like" ? "likes" : "bought"}<Link to={`nft/${nft.name}`}>{nft.name}</Link>
-            <Link to={`nft/user/${nft.user.name}`}>{nft.user.name}</Link>
-            </p>
-            <p>{formatDistance(parseISO(created_at), new Date(), {
-                    addSuffix: true,
-                })}</p>
+            <Avatar url={user.avatar.url} verified={user.verified} />
+            <p>{user.username}{type === "like" ? "likes" : "bought"}<Link href={`/nft/${nft.name}`}>{nft.name}</Link> 
+            <Link href={`/nft/owner/${nft.owner.username}`}>{nft.owner.username}</Link></p> 
+            <p>{formatDistance(parseISO(created_at), new Date())} ago</p>
         </div>
     )
 }
